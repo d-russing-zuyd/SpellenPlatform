7f38843e86dbfbaf2f2805ead6187d974a3721ab
Damian Russing <damianrussing@ziggo.nl>
2023-06-16 16:56:39 +0200

    Added GameDbContext

---
+++ SpellenPlatform/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+namespace SpellenPlatform.Models
+{
+    public class GameDBContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+    }
+
+}

0d4bb4b26847b31b98e47d05de3d6956d2893e53
Damian Russing <damianrussing@ziggo.nl>
2023-06-20 16:14:06 +0200

    Added a category model for the game categories

---
+++ SpellenPlatform/Data/Migrations/20230620141351_CreateCategory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpellenPlatform.Data.Migrations
+{
+    public partial class CreateCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ SpellenPlatform/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SpellenPlatform.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public bool ShowInNav { get; set; } 
+    }
+}
